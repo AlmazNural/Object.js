6bc4e248a5d281b006fb678c9c360ecf6a4c5cf9
ALMAZ <almaz.nursultan@bk.ru>
2023-05-23 19:14:46 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// 1. Создать объект user с полями name, age и email. Вывести значения полей в консоль.
+//      -Добавить ключ id со значением 1 
+//      -Добавить ключ second name
+//      -Удалить ключ age
+//      -Изменит значение ключа email
+
+// const user = {
+//    name: "nursultan",
+//    age: 19,
+//    email: "almaz@email.com"
+// }
+//  user.id = 1
+// user["second name"] = "Almaz"
+// delete user.age
+// user.email = "nursultan@email.bk"
+// console.log(Object.keys(user));
+// console.log(user["second name"]);
+// console.log(user["email"]);
+
+// 2. Создайте объект заработных плат obj = {Ширин: '1000',
+//    Айжан:'500', Атай: '200'}; Выведите на экран зарплату Атая и Ширин.
+
+// const obj = {
+//    Ширин: "1000",
+//    Айжан: "500",
+//    Атай: "200"
+// }
+// console.log(obj.Атай);
+// console.log(obj.Ширин);
+
+// 3. Создать объект team с полем members, представляющим собой массив имен участников. 
+//    Добавить нового участника в массив и вывести обновленный объект в консоль. 
+//    (Учитываем, то что мы можем не знать список участников которые уже записаны)
+
+// const items = [{meabers : "Данияр"},{meabers : "Рустам"},{meabers : "Аяна"},{meabers : "Дулустан"},{meabers : "Света"}];
+// items.push({meabers : "Нурсултан"});
+// console.log(items);
